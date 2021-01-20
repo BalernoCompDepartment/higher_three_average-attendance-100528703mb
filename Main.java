48d9518fc3059c5741f799561f188226f06cda09
mrHendry <tom.hendry@balernochs.edin.sch.uk>
2021-01-20 13:54:39 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    
+  // Step 1: set total initially to 0
+    
+  //Step 2: Start a fixed loop for 8 atttendances
+    
+  //Step 3: ask user for attendance
+    
+  //Step 4: add value of attendance to total
+    
+  //Step 5: End fixed loop
+    
+  //Step 6: Round average and convert to integer
+    
+  //Step 7: display the "Average cricket attendance is " concantenated with the calue of the average and followed by " this year"\
+  }
+}
